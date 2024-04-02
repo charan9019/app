68c1e825e53c416e4739511d7e67ff725dd789b4
Charan <145490809+charan9019@users.noreply.github.com>
2024-04-02 13:02:42 +0530

    Update web_app.py
    add security and mapped link

--- web_app.py
+++ web_app.py
@@ -1,68 +1,96 @@
-from flask import Flask, send_from_directory
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template_string
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '''<!DOCTYPE html>
-<html lang='en'>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-  content='width=device-width,scale=1.0'>
-  <title>Charan_web</title>
-  <link rel="icon" type="image/png" href="image/thumbnail.png">
-  <style>
-  .container {
-    position:relative;
-  }
-  body {
-    background-image:url('/image/image1.png');
-    background-repeat:no-repeat;
-    background-attachment:fixed;
-    background-size:cover;
-  }
-    
-  .centered-word{
-    position:absolute;
-    font-family:'Courier New', monospace;
-    font-size:4rem;
-    transform:translate(50% ,100%);
-  }
-      
+# Dictionary to store valid usernames and passwords
+valid_credentials = {
+    "charanwb": "crn5717"
+}
 
-  input {
-    height:2vh;
-    padding:20px;
-    border-radius:20px;
-  }
-  input.input1{
-    position: absolute;
-    
-    transform: translate(50% ,400%);
-  }
-  input.input2{
-    position:absolute;
-    
-    transform:translate(50% ,550%);
-  }
-    
-  </style>
-</head>
-<body>  
-  <div class='container'>
-    <div class="centered-word">LOGIN!</div>
-    <form>
-      <input class="input1" type="text" placeholder="username">
-      <input class="input2" type="password" placeholder="password">    </form>
-  </div> 
-</body>
-</html>'''
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username in valid_credentials and valid_credentials[username] == password:
+            # Redirect to the GitHub repository if credentials are correct
+            return redirect("https://www.python.org/")
+        else:
+            # Set the error message if credentials are incorrect
+            error = 'Invalid username or password'
+
+    html = '''<!DOCTYPE html>
+    <html lang='en'>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content='width=device-width, initial-scale=1.0'>
+        <title>Charan_web</title>
+        <link rel="icon" type="image/png" href="/image/thumbnail.png">
+        <style>
+            .container {
+                position:relative;
+            }
+            body {
+                background-image:url('/image/unplash_buddha.png');
+                background-repeat:no-repeat;
+                background-attachment:fixed;
+                background-size:cover;
+            }
+            .centered-word{
+                position:absolute;
+                font-family:'Courier New', monospace;
+                font-size:4rem;
+                transform:translate(50% ,100%);
+            }
+            input {
+                height:2vh;
+                padding:20px;
+                border-radius:20px;
+            }
+            input.input1{
+                position: absolute;
+                transform: translate(50% ,400%);
+            }
+            input.input2{
+                position:absolute;
+                transform:translate(50% ,550%);
+            }
+            button {
+                height: 50px;
+                width: 100px;
+                background-color: #4CAF50;
+                color: white;
+                border-radius: 20px;
+                border: none;
+                cursor: pointer;
+                position: absolute;
+                transform: translate(-50%, 850%);
+                left: 50%;
+            }
+        </style>
+    </head>
+    <body>
+    <div class='container'>
+        <div class="centered-word">LOGIN!</div>
+        {% if error %}
+        <div style="color: red; position: absolute; transform: translate(50%, 100%);">{{ error }}</div>
+        {% endif %}
+        <form method="post">
+            <input class="input1" type="text" name="username" placeholder="username">
+            <input class="input2" type="password" name="password" placeholder="password">
+            <button type="submit">Go</button>
+        </form>
+    </div>
+    </body>
+    </html>'''
+    return render_template_string(html, error=error)
 
 @app.route('/image/<path:filename>')
 def serve_image(filename):
-    return send_from_directory('/storage/emulated/0/Documents/html/', filename)
- 
+    return send_from_directory('/storage/emulated/0/Download/', filename)
+
 @app.route("/image/thumbnail.png")
 def serv_thumbnail():
     return send_from_directory("/storage/emulated/0/Documents/html/", "thumbnail.png")
